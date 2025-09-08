99369129fab57cc642037ae3268f5013b3687a5f
Juan Pablo David <mensajesajuanpablo@gmail.com>
2025-09-08 19:34:18 -0300

    Auto-generated commit

---
+++ chunk-QMMN6CCK.js
@@ -0,0 +1,1 @@
+import{la as o,ra as n,sa as m,ua as p}from"./chunk-FTVTGPPF.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],decls:2,vars:0,template:function(e,s){e&1&&(n(0,"p"),p(1,"Hi! Welcome to my portpolio updated to 2025"),m())},encapsulation:2})};export{a as HomeComponent};
